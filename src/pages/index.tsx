83523035b00a7820821cb19a022c5b0bb1a63870
Adam Watkins <cadamrun@gmail.com>
2022-12-09 23:59:22 -0800

    Make responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-10xl font-extrabold tracking-tight text-white sm:text-[8rem]">
+          <h1 className="text-10xl font-extrabold tracking-tight text-white sm:text-[8rem] text-[5rem]">
             Estim<span className="text-[hsl(280,100%,70%)]">
             <button onClick={handleClick}>{fib}</button>
           </span>r
