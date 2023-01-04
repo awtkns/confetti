92177722efbcdb491a10a5a458cd62663e16ee7d
Adam Watkins <cadamrun@gmail.com>
2023-01-04 00:49:42 -0800

    :rocket: Add gradient to home page

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -21,7 +21,14 @@ const DefaultLayout = (props: LayoutProps) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2563eb] to-[#1e293b]">
+      <main
+        className="flex min-h-screen flex-col items-center"
+        style={{
+          backgroundColor: "rgb(30, 41, 59)",
+          backgroundImage:
+            "radial-gradient(at 100% 0%, rgb(59, 130, 246) 0, transparent 80%), radial-gradient(at 0% 5%, rgb(147, 51, 234) 0, transparent 50%);",
+        }}
+      >
         <Header />
         {props.children}
       </main>
